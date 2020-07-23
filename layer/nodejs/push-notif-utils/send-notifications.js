d5cb3474637f3d87c5efda81eb107207e3e2be48
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-07-23 12:52:57 +0200

    Handling deregistration on send failure

--- layer/nodejs/push-notif-utils/send-notifications.js
+++ layer/nodejs/push-notif-utils/send-notifications.js
@@ -1,14 +1,60 @@
 const webpush = require('web-push')
 
-module.exports = (res) => {
-  const [subs, keys] = res
+module.exports = (data) => {
+  const [users, keys] = data
   webpush.setVapidDetails(
     'mailto:a.lemaire@klimapartner.de',
     keys.publicKey,
     keys.privateKey
   )
-  const payload = {
-    message: 'Hello World!'
+  return Promise.allSettled(users.map(notifyUser))
+}
+
+function notifyUser (user) {
+  const payload = JSON.stringify({ message: 'Hello World!' })
+  const sendNotif2Sub = sub => webpush.sendNotification(sub, payload).catch(err => err.statusCode === 410 ? deregisterSub(user, sub) : err)
+  return Promise.allSettled(user.subscriptions.map(sendNotif2Sub))
+}
+
+function deregisterSub (user, sub) {
+  const es = require('aws-es-client')({
+    id: process.env.ES_ID,
+    token: process.env.ES_SECRET,
+    url: process.env.ES_ENDPOINT
+  })
+  return es.updateByQuery({
+    index: 'users',
+    refresh: true,
+    body: {
+      script: getScript(sub),
+      query: getQuery(user, sub)
+    }
+  })
+}
+
+function getScript (sub) {
+  return {
+    lang: 'painless',
+    source: 'ctx._source.subscriptions.remove(ctx._source.subscriptions.indexOf(params.sub))',
+    params: {
+      sub: sub
+    }
   }
-  return Promise.all(subs.map(sub => webpush.sendNotification(sub, JSON.stringify(payload))))
+}
+
+function getQuery (user, sub) {
+  return {
+    bool: {
+      must: {
+        match: {
+          _id: user
+        }
+      },
+      filter: {
+        match: {
+          'subscriptions.endpoint': sub.endpoint
+        }
+      }
+    }
+  }
 }
