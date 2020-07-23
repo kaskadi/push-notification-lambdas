156d5c74ad0e0e8e93be4f2ee52f5b97e8545847
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-07-23 12:42:38 +0200

    Updated utility name in main file

--- lambdas/stocks-notif/stocks-notif.js
+++ lambdas/stocks-notif/stocks-notif.js
@@ -1,6 +1,6 @@
-const { getSubs, getKeys, sendNotifications } = require('push-notif-utils')
+const { getUsers, getKeys, sendNotifications } = require('push-notif-utils')
 
 module.exports.handler = async (event) => {
-  return await Promise.all([getSubs(), getKeys()])
+  return await Promise.all([getUsers(), getKeys()])
     .then(sendNotifications)
 }
