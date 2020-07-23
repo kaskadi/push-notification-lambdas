e7bb475e4eb33af3168156bce1d159c61117a3ae
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-07-23 14:39:07 +0200

    Moved deregistering logic out of send notif util

--- layer/nodejs/push-notif-utils/send-notifications.js
+++ layer/nodejs/push-notif-utils/send-notifications.js
@@ -10,51 +10,10 @@ module.exports = (data) => {
   return Promise.allSettled(users.map(notifyUser))
 }
 
-function notifyUser (user) {
+async function notifyUser (user) {
   const payload = JSON.stringify({ message: 'Hello World!' })
-  const sendNotif2Sub = sub => webpush.sendNotification(sub, payload).catch(err => err.statusCode === 410 ? deregisterSub(user, sub) : err)
-  return Promise.allSettled(user.subscriptions.map(sendNotif2Sub))
-}
-
-function deregisterSub (user, sub) {
-  const es = require('aws-es-client')({
-    id: process.env.ES_ID,
-    token: process.env.ES_SECRET,
-    url: process.env.ES_ENDPOINT
-  })
-  return es.updateByQuery({
-    index: 'users',
-    refresh: true,
-    body: {
-      script: getScript(sub),
-      query: getQuery(user, sub)
-    }
-  })
-}
-
-function getScript (sub) {
   return {
-    lang: 'painless',
-    source: 'ctx._source.subscriptions.remove(ctx._source.subscriptions.indexOf(params.sub))',
-    params: {
-      sub: sub
-    }
-  }
-}
-
-function getQuery (user, sub) {
-  return {
-    bool: {
-      must: {
-        match: {
-          _id: user
-        }
-      },
-      filter: {
-        match: {
-          'subscriptions.endpoint': sub.endpoint
-        }
-      }
-    }
+    user: user.email,
+    pushes: await Promise.allSettled(user.subscriptions.map(sub => webpush.sendNotification(sub, payload)))
   }
 }
