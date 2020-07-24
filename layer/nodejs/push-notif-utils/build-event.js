5de7219598a305e0d6270b0e07ee5fb6905decdf
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-07-24 16:10:56 +0200

    Added logging for monitoring

--- layer/nodejs/push-notif-utils/build-event.js
+++ layer/nodejs/push-notif-utils/build-event.js
@@ -1,4 +1,5 @@
 module.exports = deadNotifs => {
+  console.log(JSON.stringify(deadNotifs, null, 2))
   return {
     hasDeadEndpoints: String(deadNotifs.length > 0), // as for now (24/07/2020) AWS CloudFormation is transforming Boolean/Numbers in EventBridge pattern to strings so we're doing the same here to make sure the event is caught
     deadNotifs
